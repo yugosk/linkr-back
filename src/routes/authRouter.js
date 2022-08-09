32ca8c0329e7799e2ee8d4a5a88159608eca618e
Mary Karakida <makarakida@gmail.com>
2022-08-09 13:45:23 -0300

    feat: create sign in feature

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 import { schemaMiddleware } from "../middlewares/schemaMiddleware.js/schemaMiddleware.js";
 import { signUp, signIn } from "../controllers/authController.js";
 
-import { newUserSchema } from "../schemas/authSchema.js";
+import { newUserSchema, userSchema } from "../schemas/authSchema.js";
 
 const router = Router();
 
 router.post("/signup", schemaMiddleware(newUserSchema), signUp);
-router.post("/signin", signIn);
+router.post("/signin", schemaMiddleware(userSchema), signIn);
 
 export default router;
