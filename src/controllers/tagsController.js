3ee64f4ae1d17f6eb8057de768a32e385e66f42b
Cecilia Salema <ceciliasiman15@gmail.com>
2022-08-11 12:14:16 -0300

    fix status

--- src/controllers/tagsController.js
+++ src/controllers/tagsController.js
@@ -3,7 +3,7 @@ import { trendingHashtags, postsWithTag } from "../repositories/tagsRepository.j
 export async function trending(req,res){
     try{
         const listTrendig = await trendingHashtags();
-        res.status(201).send(listTrendig);
+        res.status(200).send(listTrendig);
     }
     catch(error){
         res.status(500).send('Unable to obtain trending hashtags');
@@ -13,9 +13,12 @@ export async function trending(req,res){
 
 export async function hashtagPage(req,res){
     const { hashtag } = req.params;
+    if (!hashtag){
+        return res.sendStatus(404);
+    }
     try{
         const listPosts  = await postsWithTag(hashtag);
-        res.status(201).send(listPosts);
+        res.status(200).send(listPosts);
     }
     catch(error){
         res.status(500).send('Unable to obtain posts for hashtag');
