109824efe2048ca88115dbe1b02a32a272f37844
Cecilia Salema <ceciliasiman15@gmail.com>
2022-08-12 22:57:38 -0300

    feat: edit and delete

---
+++ src/controllers/alterPostController.js
@@ -0,0 +1,40 @@
+import { checkOwner, edit, exclude } from "../repositories/alterPostRepository.js";
+
+export async function editPost(req,res){
+    const { postId, newDescription } = req.body;
+    let { id } = res.locals.userData;
+    try {
+        let { userId } = await checkOwner(postId);
+        userId = parseInt(userId);
+        id = parseInt(id);
+        if (userId !== id){
+            return res.status(401).send('Post does not belong to user');
+        }
+        await edit(postId,newDescription);
+        res.status(200).send("Post edited");
+    }catch(err){
+        res.status(500).send("Unable to edit post");
+    } 
+
+}
+
+export async function deletePost(req,res){
+    const { postId } = req.body;
+    if (!postId){
+        return res.sendStatus(404);
+    }
+    let { id } = res.locals.userData;
+    try {
+        let {userId} = await checkOwner(postId);
+        userId = parseInt(userId);
+        id = parseInt(id);
+        if (userId !== id){
+            return res.status(401).send('Post does not belong to user');
+        }
+        await exclude(postId);
+        res.status(200).send("Post deleted");
+    }catch(err){
+        res.status(500).send("Unable to delete post");
+    } 
+
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import authRouter from "./authRouter.js";
 import tagsRouter from "./tagsRouter.js"
 import postsRouter from "./postsRouter.js";
 import usersRouter from "./usersRouter.js";
+import alterPostRouter from "./alterPostRouter.js";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/", authRouter);
 router.use("/",tagsRouter);
 router.use("/", postsRouter);
 router.use("/users", usersRouter);
+router.use("/",alterPostRouter);
 
 export default router;
