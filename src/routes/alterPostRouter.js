254a6867b1a424681fe8444fe651936ac9f0fe6b
Mary Karakida <makarakida@gmail.com>
2022-08-15 12:53:25 -0300

    fix: change schemaMiddleware import path in alterPostRouter file

--- src/routes/alterPostRouter.js
+++ src/routes/alterPostRouter.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 import { deletePost, editPost } from "../controllers/alterPostController.js";
 import { validateToken } from "../middlewares/authMiddleware/authMiddleware.js";
-import { schemaMiddleware } from "../middlewares/schemaMiddleware.js/schemaMiddleware.js";
+import { schemaMiddleware } from "../middlewares/schemaMiddleware/schemaMiddleware.js";
 import { editSchema } from "../schemas/editSchema.js";
 
 const router = Router();
-router.put("/editing",validateToken,schemaMiddleware(editSchema), editPost);
-router.delete("/deleting/:postId",validateToken,deletePost);
+router.put("/editing", validateToken, schemaMiddleware(editSchema), editPost);
+router.delete("/deleting/:postId", validateToken, deletePost);
 
 export default router;
