560b0fc3d94b0ef2dc2e868cfe60a77ef064f251
Mary Karakida <makarakida@gmail.com>
2022-08-16 15:34:30 -0300

    fix: send correct parameters to findFollow function

--- src/controllers/followersController.js
+++ src/controllers/followersController.js
@@ -20,10 +20,10 @@ export async function createNewFollower(req, res) {
       return res.status(404).send("Cannot follow a user who does not exist");
     }
 
-    const { rowCount: followRowCount } = await findFollow(
+    const { rowCount: followRowCount } = await findFollow({
       followedId,
-      followerId
-    );
+      followerId,
+    });
 
     if (followRowCount !== 0) {
       return res.status(409).send("Already following this user");
@@ -52,10 +52,10 @@ export async function deleteFollower(req, res) {
       return res.status(404).send("Cannot unfollow a user who does not exist");
     }
 
-    const { rowCount: followRowCount } = await findFollow(
+    const { rowCount: followRowCount } = await findFollow({
       followedId,
-      followerId
-    );
+      followerId,
+    });
 
     if (followRowCount === 0) {
       return res.status(409).send("Not following this user yet");
