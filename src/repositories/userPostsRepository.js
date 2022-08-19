dd543aca4b3d441c3f1bbce235eb9ff42ff58fcf
Cecilia Salema <ceciliasiman15@gmail.com>
2022-08-19 18:38:41 -0300

    fix: return for userPosts

--- src/repositories/userPostsRepository.js
+++ src/repositories/userPostsRepository.js
@@ -1,15 +1,15 @@
 import connection from "../databases/pgsql.js";
 
 export async function postsOfUser(userId){
-    let { rows:listPosts } = await connection.query(
+    const { rows:listPosts } = await connection.query(
     `SELECT users.picture, users.username, posts.id, posts.url, posts.description, posts."userId", posts."createdAt" FROM posts 
     JOIN users ON users.id = posts."userId"
     WHERE posts."userId"=$1`,[userId]);
     if (listPosts.length === 0){
-        const { rows:listPosts } = await connection.query(
+        const { rows:userData } = await connection.query(
         `SELECT users.picture, users.username FROM users
         WHERE id=$1`,[userId]);
-        return (listPosts);
+        return (userData);
     }
     return (listPosts);
 }
